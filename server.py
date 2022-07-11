8992720a3e0a524263702f81ec29a61cf8350f9d
root <aimen_r@yahoo.fr>
2022-07-11 14:57:11 -0400

    first file

---
+++ server.py
@@ -0,0 +1,18 @@
+import os
+import platform
+from flask import Flask
+from cfenv import AppEnv
+
+
+app = Flask(__name__)
+env = AppEnv()
+
+@app.route('/')
+def hello():
+    
+    print(platform.system())
+
+    
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=int(os.getenv("PORT", 5000)))
